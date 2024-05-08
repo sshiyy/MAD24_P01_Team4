2b9f4084fc2c7be40c69e7ba6542043521add945
RaeannTaiYuXuan <S10262832@connect.np.edu.sg>
2024-05-08 21:36:25 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,11 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+
 }
+

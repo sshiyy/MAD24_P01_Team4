bf732530bf0dc6db0438428457e257573f7e7be5
sshiyy <s10257952@connect.np.edu.sg>
2024-05-17 16:58:27 +0800

    slight changes

--- app/src/main/java/sg/edu/np/mad/mad_p01_team4/ProductPage.java
+++ app/src/main/java/sg/edu/np/mad/mad_p01_team4/ProductPage.java
@@ -1,6 +1,9 @@
 package sg.edu.np.mad.mad_p01_team4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProductPage extends AppCompatActivity {
+
+    private TextView tvQuantity;
+    private ImageButton btnIncrease;
+    private ImageButton btnDecrease;
+    private int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,41 @@ public class ProductPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tvQuantity = findViewById(R.id.tv_quantity);
+        btnIncrease = findViewById(R.id.btn_increase);
+        btnDecrease = findViewById(R.id.btn_decrease);
+
+        tvQuantity.setText(String.valueOf(quantity));
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseQuantity();
+            }
+        });
+
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decreaseQuantity();
+            }
+        });
+
+    }
+
+    public void increaseQuantity() {
+        quantity++;
+        tvQuantity.setText(String.valueOf(quantity));
+    }
+
+    public void decreaseQuantity() {
+        if (quantity>0){
+            quantity--;
+            tvQuantity.setText(String.valueOf(quantity));
+        }
     }
 }
+
+
+

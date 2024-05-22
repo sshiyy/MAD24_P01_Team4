4c48b27d98693e4e3c5762a19df9d108e411ce3c
Jason Ng <S10262552@connect.np.edu.sg>
2024-05-22 14:33:51 +0800

    Added Firebase Stuff

--- app/src/main/java/sg/edu/np/mad/mad_p01_team4/Login_Page.java
+++ app/src/main/java/sg/edu/np/mad/mad_p01_team4/Login_Page.java
@@ -8,54 +8,122 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
 public class Login_Page extends AppCompatActivity {
+
+    EditText username, password;
+    Button login_btn;
+
+    TextView signupRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
 
-        Button login_btn = findViewById(R.id.btn_login);
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_password);
+        login_btn = findViewById(R.id.btn_login);
+        signupRedirectText = findViewById(R.id.tv_signup);
+
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get references to EditTexts
-                EditText username = findViewById(R.id.et_username);
-                EditText password = findViewById(R.id.et_password);
+                if(!validatePassword() | !validateUsername() ){
 
-                // Get the text entered in EditTexts
-                String usernameText = username.getText().toString().trim();
-                String passwordText = password.getText().toString().trim();
-
-                // Check if username or password is empty
-                if (usernameText.isEmpty() || passwordText.isEmpty()) {
-                    // Show a Toast message (or any other popup)
-                    Toast.makeText(v.getContext(), "Username or Password cannot be empty!",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    // Username and password are not empty, proceed with login logic (optional)
-                    // ... (e.g., validate credentials with server)
-
-                    // Start Product Page ( ADD THIS BACK )
-                    Intent intent = new Intent(v.getContext(), productpage.class);
-                    v.getContext().startActivity(intent);
+                }else {
+                    checkUser();
                 }
             }
         });
 
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Page.this, Register_Page.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public boolean validateUsername(){
+        String val = username.getText().toString();
+        if (val.isEmpty()){
+            username.setError("Username cannot be empty");
+            return false;
+        }else {
+            username.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validatePassword(){
+        String valpass = password.getText().toString();
+        if (valpass.isEmpty()){
+            password.setError("Password cannot be empty");
+            return false;
+        }else {
+            password.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String userUsername = username.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()){
+                    username.setError(null);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+
+                    if (!Objects.equals(passwordFromDB, password)) {
+                        username.setError(null);
+                        Intent intent = new Intent(Login_Page.this, productpage.class);
+                        startActivity(intent);
+                    } else {
+                        password.setError("Invalid");
+                        password.requestFocus();
+                    }
+                }else {
+                    username.setError("User does not exist");
+                    username.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
 
     }
 }

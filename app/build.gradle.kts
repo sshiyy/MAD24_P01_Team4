c3818d022cb1bf853aa684352f807a7da2524ee8
Jason Ng <S10262552@connect.np.edu.sg>
2024-05-23 00:21:13 +0800

    Added FireStore for Users and FireBase Authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore")
 
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

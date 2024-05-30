61d86fb21893c80f4988324238e1650371d62df4
sshiyy <148183319+sshiyy@users.noreply.github.com>
2024-05-31 02:27:48 +0800

    Update cart.java

--- app/src/main/java/sg/edu/np/mad/mad_p01_team4/cart.java
+++ app/src/main/java/sg/edu/np/mad/mad_p01_team4/cart.java
@@ -1,13 +1,16 @@
 package sg.edu.np.mad.mad_p01_team4;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class cart {
     private static cart instance;
     private List<Food> cartitems;
 
     private cart() {
+
         cartitems = new ArrayList<>();
     }
 
@@ -17,27 +20,29 @@ public class cart {
         }
         return instance;
     }
+
     public void additems(Food food) {
         for (Food item : cartitems) {
             if (item.getName().equals(food.getName())) {
-                item.incrementQuantity();
+                item.setQuantity(item.getQuantity() + food.getQuantity());
                 return;
             }
         }
         cartitems.add(food);
     }
 
+    public void updateCart(Food food) {
+        for (Food item : cartitems) {
+            if (item.getName().equals(food.getName())) {
+                item.setQuantity(food.getQuantity());
+                return;
+            }
+        }
+    }
+
     public List<Food> getCartitems() {
         return cartitems;
     }
 
-    public void clearCart() {
-        cartitems.clear();
-    }
-
-    public boolean isCartempty() {
-        return cartitems.isEmpty();
-    }
-
-
 }
+

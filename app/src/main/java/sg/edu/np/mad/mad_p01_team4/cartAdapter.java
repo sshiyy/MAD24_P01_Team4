72d442bb0cb44bbe19636a970bff8b8d55baca1a
Ming Qi <s10257808@connect.np.edu.sg>
2024-05-31 20:46:38 +0800

    Cartpage and payment completed

--- app/src/main/java/sg/edu/np/mad/mad_p01_team4/cartAdapter.java
+++ app/src/main/java/sg/edu/np/mad/mad_p01_team4/cartAdapter.java
@@ -12,9 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import com.bumptech.glide.Glide;
-
 import java.util.List;
 
 public class cartAdapter extends RecyclerView.Adapter<cartAdapter.cartViewHolder> {

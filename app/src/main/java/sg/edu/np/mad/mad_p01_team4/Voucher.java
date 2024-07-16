1e72841d5b0861134965c84e09579788cd86b7d5
RaeannTaiYuXuan <150000973+RaeannTaiYuXuan@users.noreply.github.com>
2024-07-17 04:51:27 +0800

    R: Made the points and voucher works but cannot display in cart yet,, wil update again

--- app/src/main/java/sg/edu/np/mad/mad_p01_team4/Voucher.java
+++ app/src/main/java/sg/edu/np/mad/mad_p01_team4/Voucher.java
@@ -1,15 +1,30 @@
 package sg.edu.np.mad.mad_p01_team4;
 
 public class Voucher {
-    public String name;
-    public int pointCost;
-    public String expiryDate;
-    public boolean redeemed;
-    public Voucher(String name, int pointCost, String expiryDate, boolean redeemed){
-        this.name = name;
-        this.pointCost = pointCost;
-        this.expiryDate = expiryDate;
-        this.redeemed = redeemed;
+    private String title;
+    private String discount;
+
+    // Required for Firestore serialization
+    public Voucher() {}
+
+    public Voucher(String title, String discount) {
+        this.title = title;
+        this.discount = discount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
     }
 }
-
